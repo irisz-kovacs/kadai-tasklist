e1d648127eca14284514a23aa7c54fa64adb83f9
irisz-kovacs <ec2-user@ip-172-31-17-78.us-east-2.compute.internal>
2018-05-31 05:22:14 +0000

    kadai-tasklist CRUD messages

---
+++ resources/views/tasks/create.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Create new Task</h1>
+
+    {!! Form::model($task, ['route' => 'tasks.store']) !!}
+
+        {!! Form::label('content', 'Task:') !!}
+        {!! Form::text('content') !!}
+
+        {!! Form::submit('Post') !!}
+
+    {!! Form::close() !!}
+
+@endsection
---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>Task List</h1>
+
+    @if (count($tasks) > 0)
+        <ul>
+            @foreach ($tasks as $task)
+                <li>{{ $task->content }}</li>
+            @endforeach
+        </ul>
+    @endif
+    
+ @foreach ($tasks as $task)
+                <li>{!! link_to_route('tasks.show', $task->id, ['id' => $task->id]) !!} : {{ $task->content }}</li>
+                
+    {!! link_to_route('tasks.create', 'Post new Task') !!}
+    
+@endsection
